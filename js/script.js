31362005189a9051b6d7137d1491fef3604d9596
Nikitronus <Slimehide@gmail.com>
2020-10-06 13:10:57 +0300

    Almost there version

--- js/script.js
+++ js/script.js
@@ -1,14 +1,51 @@
 $(document).ready(function(){
 
-
-	$(".drop__li>a").on("focus" ,function(e){
-		e.preventDefault();
+	$(".menu__button").on("click" ,function(e){
+		if ($(this).hasClass("active__button")) {
+			$("body,html").css("overflow-y" ,"auto");
+			$(".center__list").css("top" ,"-100vh");
+			$(this).removeClass("active__button");
+			if ($(window).width() < 640) {
+				$(".header__buttons").css("top" , "-100vh");
+			}
+		} else {
+			$("html, body").animate({ scrollTop: 0 }, "slow");
+			$(".center__list").css("top" ,"0px");
+			$(this).addClass("active__button");
+			$("body,html").css("overflow-y" ,"hidden");
+			if ($(window).width() < 640) {
+				$(".header__buttons").css("top" , "0");
+			}
+		}
+	});
+	$(".drop__li>a").on("blur" ,function(e){
+		if ($(window).width() < 991) {
+			$(this).removeClass("opened__drop");
+		}
+	});
+	$(".drop__li>a").on("click" ,function(e){
+		if ($(window).width() < 991) {
+			if ($(this).hasClass("opened__drop")) {
+				$(this).removeClass("opened__drop");
+			} else {
+				e.preventDefault();
+				$(this).addClass("opened__drop");
+			}
+		}
 	});
 	$(".drop__li").on("mouseenter" , function(){
-		$(this).find(".dropdown__block").fadeIn(300);
+		if ($(window).width() > 991) {
+			$(this).find(".dropdown__block").fadeIn(300);
+		} else {
+			$(this).find(".dropdown__block").slideDown(300);
+		}
 	});
 	$(".drop__li").on("mouseleave" , function(){
-		$(this).find(".dropdown__block").fadeOut(300);
+		if ($(window).width() > 991) {
+			$(this).find(".dropdown__block").fadeOut(300);
+		} else {
+			$(this).find(".dropdown__block").slideUp(300);
+		}
 	});
 	$(".head__certificate").on("click" ,function(e){
 		e.preventDefault();
